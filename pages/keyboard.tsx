7fd8bf189a283eee33e7541f92e74e21d07ffb91
mks <47758224+dogfrogfog@users.noreply.github.com>
2022-07-30 17:09:00 +0200

    feat: add /keyboard page (#10)

---
+++ pages/keyboard.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+
+import styles from '../styles/keyboard.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <span className="notDone notWhite">commented js handles all logic</span>
+      <div className={styles.container}>
+        ____
+      </div>
+    </>
+  )
+}
+
+export default Home
